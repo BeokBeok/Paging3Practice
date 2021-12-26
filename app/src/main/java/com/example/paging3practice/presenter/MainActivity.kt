1cae2372e38a5a3df711bfba3026aa4dd7f7685d
강현석 <kekemusa37@gmail.com>
2021-12-26 19:39:10 +0900

    :sparkles: [적용] 처음 데이터 불러올 때 로딩바 표시

--- app/src/main/java/com/example/paging3practice/presenter/MainActivity.kt
+++ app/src/main/java/com/example/paging3practice/presenter/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.paging3practice.presenter
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.RecyclerView
 import com.example.paging3practice.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,6 +36,15 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             viewModel.pagingData.collectLatest(reposAdapter::submitData)
+        }
+
+        lifecycleScope.launch {
+            reposAdapter.loadStateFlow.collect { loadState ->
+                val isEmptyList = loadState.refresh is LoadState.NotLoading && reposAdapter.itemCount == 0
+                binding.rvMain.isVisible = !isEmptyList
+                binding.pbMain.isVisible = loadState.source.refresh is LoadState.Loading
+                binding.btnMainRetry.isVisible = loadState.source.refresh is LoadState.Error
+            }
         }
     }
 
